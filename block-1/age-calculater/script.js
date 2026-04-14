f689274ac283e6b3c42bc591b3d2d0d6e4851e06
Sergey <gavps@vk.com>
2026-04-14 20:04:34 +0600

    fix(block-1): fix remove undefined variable

--- block-1/age-calculater/script.js
+++ block-1/age-calculater/script.js
@@ -6,7 +6,7 @@ let age = 0;
 
 yearOfBirth = Number(prompt("Какой у вас год рождения?")); // просим пользователя написать его год рождения
 
-if (yearOfBirth === null || yearOfBirth.trim() === "" || Number.isNaN(yearOfBirth) ) { // проверяем коректность ввода, если вод не коректен выдаем ошибку
+if (yearOfBirth === null || Number.isNaN(yearOfBirth)) { // проверяем коректность ввода, если вод не коректен выдаем ошибку
     alert("Ошибка: Некоректный ввод.");
     console.warn("Пожалуйста, введите корректный год рождения (число).");
 } else {
